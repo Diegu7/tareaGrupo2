167105403cd7b913e4ebed5216055be497636afc
Diegu7 <mecayounbloque@gmail.com>
2016-10-21 09:30:49 -0600

    fuck

--- src/main/SalaCine.java
+++ src/main/SalaCine.java
@@ -9,14 +9,52 @@ public class SalaCine {
 	public void arrDec(){
 		for(int f = 0; f < arrTicket.length; f++){
 			for(int c = 0; c < arrTicket[f].length; c++){
-				arrTicket[f][c] = new Ticket(f,c);
+				arrTicket[f][c] = new Ticket(0,0,null);
 			}
 		}
 	}
 	
-	public boolean isPalindromo(String nombre){
-		return true;
+	public Coordenada searchTicketByReference(int number, int fila,int columna){
+        if(fila<=arrTicket.length){
+            if(columna <= arrTicket[fila].length){
+                if(arrTicket[fila][columna].getRef()==number)
+                    return new Coordenada(fila,columna);
+                return searchTicketByReference(number,fila,columna+1); 
+            }
+            searchTicketByReference(number,fila+1,1);
+        }
+        return null;
+    }
+	
+	private double income(int f, int c, double inc){
+		if(f < arrTicket.length){
+			if(c < arrTicket[f].length){
+				inc = inc + + arrTicket[f][c].getPrecio();
+				return income(f, c+1, inc);
+			}
+			return income(f+1, 0, inc);
+		}
+		return 0;
 	}
+	
+	public double income(){
+		return income(0,0,0);
+	}
+	
+	private boolean isPalindromo(String name, int contador1,int contador2){
+	       if(name.length()>contador1){
+	          if(name.charAt(contador1)== name.charAt(name.length()-contador2))
+	              isPalindromo(name,contador1+1,contador2+1);
+	          else
+	              return false;
+	       }
+	       return true;
+	   }
+
+
+	public boolean isPalindromo(String name){
+	    return this.isPalindromo(name, 0, 1);
+	 }
 	
 	private void reset(int f, int c){
 		if(f < arrTicket.length){
@@ -57,18 +95,18 @@ public class SalaCine {
 		if(f < arrTicket.length){
 			if(c < arrTicket[f].length){
 				if(arrTicket[f][c].getFecha() == null){
-					System.out.print("[DISPONIBLE]");
-					reset(f, c+1);
+					System.out.print("[*D I S P O N I B L E]");
+					printSala(f, c+1);
 					return;
 				}
 				System.out.print("[");
 				arrTicket[f][c].print();
-				System.out.print("] ");
-				reset(f, c+1);
+				System.out.print("]");
+				printSala(f, c+1);
 				return;
 			}
 			System.out.println();
-			reset(f+1, 0);
+			printSala(f+1, 0);
 		}
 	}
 	
